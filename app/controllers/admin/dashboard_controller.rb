d8bec6fd613baf969bef019b933d0e5cea8aa652
Ashley <ashley.barr@ar4s.com>
2021-02-07 22:32:22 -0700

    add generator for categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
+
   def show
     @product_counts = Product.count(:id)
     @category_counts = Category.count(:id)
