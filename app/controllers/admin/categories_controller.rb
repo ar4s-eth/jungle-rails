cdf43195c18e4e06b00bcf2b0dfbbc9a32f0d7e5
Ashley <ashley.barr@ar4s.com>
2021-02-08 00:23:16 -0700

    add admin namespace && implement categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  # http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["PASSWORD"]
   def index
     @categories = Category.order(id: :desc).all
   end
@@ -9,6 +10,15 @@ class Admin::CategoriesController < ApplicationController
 
    def create
     @category = Category.new(category_params)
+
+     if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
+   def category_params
+    params.require(:category).permit(:name)
+  end
 end
