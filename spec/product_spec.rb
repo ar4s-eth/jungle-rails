0f38e136888f78f3aaed2176191ee815cae5f865
Ashley <ashley.barr@ar4s.com>
2021-02-08 18:22:53 -0700

    add username test

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,5 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    it 'should create a new username that is valid' do
+      @user = User.create(
+        name: 'Steve Jobs',
+        email: 'sjobs@apple.com',
+        password: 'iheartUX',
+        password_confirmation: 'iheartUX'
+        )
+        expect(@user).to be_valid
+    end
   end
-  
+end
+
+
