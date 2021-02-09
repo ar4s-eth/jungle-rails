1e0e2d52881cdee0ae0b830eb294be36227eb0cb
Ashley <ashley.barr@ar4s.com>
2021-02-08 19:09:08 -0700

    add price test

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    
+
     it 'should create a new username that is valid' do
       @user = User.create(
         name: 'Steve Jobs',
@@ -17,6 +17,12 @@ RSpec.describe Product, type: :model do
       @category = Category.create(name: "Appliances")
       @product = Product.create(price: 599, quantity: 10, category: @category)
       expect(@product.errors.full_messages).to include "Name can't be blank"
+    end
+
+    it "should error if a price is missing" do
+      @category = Category.create(name: "Fruits")
+      @product = Product.create(name: "athousandants", quantity: 5, category: @category)
+      expect(@product.errors.full_messages).to include "Price can't be blank"
     end
 
   end
