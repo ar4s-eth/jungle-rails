7ebf301c62f395930846b1396ada2e132ea5b7fb
Ashley <ashley.barr@ar4s.com>
2021-02-09 18:23:18 -0700

    add user test

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
       expect(@user).to be_valid
   end
 
-  it 'should error if a user already exists' do
+  it 'should error if an email already exists' do
     @user = User.create(
       name: 'Steve Jobs',
       email: 'sjobs@apple.com',
@@ -23,10 +23,10 @@ RSpec.describe User, type: :model do
     @user.save
 
     @user = User.create(
-      name: 'Steve Jobs',
+      name: 'Steve Juabs',
       email: 'sjobs@apple.com',
-      password: 'iheartUX',
-      password_confirmation: 'iheartUX'
+      password: 'iheartUI',
+      password_confirmation: 'iheartUI'
       )
     @user.save
     expect(@user.errors.full_messages).to include("Email has already been taken")
@@ -40,8 +40,19 @@ RSpec.describe User, type: :model do
       password: 'bball',
       password_confirmation: 'bball'
     )
-    @user.save!
+    @user.save
     expect(@user.name).to include("Larry Bird")
+  end
+
+  it 'should error if passwords do not match' do 
+    @user = User.create(
+      name: 'Larry Bird',
+      email: 'birdman@nba.com',
+      password: 'bball',
+      password_confirmation: 'bal'
+    )
+    @user.save
+    expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
   end
 
 
